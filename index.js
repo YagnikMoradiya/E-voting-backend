d724a5bfd01f2cb177cfc17c96a217abd5bc559a
rajmangukiya <rajmangukiya00@gmail.com>
2022-09-21 21:57:47 +0530

    last change

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000'
+  }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
